e5759e3d395c1827e8e40804d70ce07c5a74bfe4
Alander <alandermm@hotmail.com>
2017-10-20 16:59:50 -0200

    Inicio projeto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SistemaVendas
 {
@@ -6,7 +7,30 @@ namespace SistemaVendas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
+            Console.WriteLine("Escola uma das opções abaixo\n"
+                        + "1 - Cadastrar Clientes\n"
+                        + "2 - Cadastrar Produtos\n"
+                        + "3 - Realizar Vendas\n"
+                        + "4 - Extrato Clientes\n"
+                        + "0 - Sair\n");
+            
+            Console.Write("Opção: ");
+            int opt = 0;            
+            do{
+                op2 = Console.ReadLine();
+                opt = Int16.Parse(op2);
+            } while (opt < 0 || opt > 6);
+
+            switch(op2){
+                case "0": Environment.Exit(0); break;
+                case "1": validarCPF(); break;
+                case "2": validarCNPJ(); break;
+                case "3": validarCredito(); break;
+                case "4": validarRG(); break;
+                case "5": validarTitulo(); break;
+                }
+            } while(op2 != "0");
         }
     }
 }
